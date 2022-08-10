09d95f9b9b6d828cfc78b42ae183abc95b053b25
Nivita <nnp25@case.edu>
2022-08-10 00:37:19 -0400

    playing with the timer function to reset time

--- script.js
+++ script.js
@@ -58,4 +58,42 @@ function showContent() {
   for (var i = 0; i < content.length; i++) {
     content[i].classList.remove("floor-"+floor);
   }
+
+var today = new Date();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+timer();
+
+function timer(){
+var currentTime = new Date()
+var hours = currentTime.getHours()
+var minutes = currentTime.getMinutes()
+var sec = currentTime.getSeconds()
+if (minutes < 10){
+    minutes = "0" + minutes
+}
+if (sec < 10){
+    sec = "0" + sec
+}
+var t_str = hours + ":" + minutes + ":" + sec + " ";
+
+if(hours > 11){
+    t_str += "PM";
+
+  } else {
+   t_str += "AM";
+}
+ document.getElementById('time_span').innerHTML = t_str;
+ setTimeout(timer,1000);
+}
+
+/*
+let day = dt.getDay();
+let month = dt.getMonth();
+let year = dt.getYear();
+
+document.getElementById('day').innerHTML = day;
+document.getElementById('month').innerHTML = month;
+document.getElementById('year').innerHTML = year;*/
+
 }
